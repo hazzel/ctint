41ba7c59c7e34ae508c285140d6d218c5a7c5ba5
Stephan Heßelmann <hesselmann@physik.rwth-aachen.de>
2016-02-22 17:38:26 +0100

    update

---
+++ src/parameters.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct parameters
+{	
+	double beta, V, zeta2, zeta4;
+	int worm_nhood_dist, n_matsubara, n_discrete_tau;
+	double ratio_w2, ratio_w4;
+	//Proposal probabilities
+	std::vector<double> add;
+	std::vector<double> rem;
+	double W2toZ, ZtoW2, ZtoW4, W4toZ, W2toW4, W4toW2, worm_shift;
+};
